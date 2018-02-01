8d738b568f9e863d443ec0483d3a391ef75a4aac
prituladima <prituladima@gmail.com>
2018-02-01 21:27:20 +0200

    CountdownRunner

---
+++ src/multithreading/countdown/CountdownRunner.java
@@ -0,0 +1,21 @@
+package multithreading.countdown;
+
+import java.util.concurrent.CountDownLatch;
+
+public class CountdownRunner {
+
+    public static void main(String[] args) throws InterruptedException {
+
+        CountDownLatch latch = new CountDownLatch(3);
+
+        Waiter waiter = new Waiter(latch);
+        Decrementer decrementer = new Decrementer(latch);
+
+        new Thread(waiter).start();
+        new Thread(decrementer).start();
+
+        Thread.sleep(4000);
+
+    }
+
+}
