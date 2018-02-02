995f5cc91e3718a4ac64d56e9b9599328bb755cb
prituladima <prituladima@gmail.com>
2018-02-02 16:22:22 +0200

    ForkJoinRunner

---
+++ src/multithreading/forkjoin/ForkJoinRunner.java
@@ -0,0 +1,14 @@
+package multithreading.forkjoin;
+
+import java.util.concurrent.ForkJoinPool;
+
+public class ForkJoinRunner {
+
+    public static void main(String[] args) {
+        ForkJoinPool forkJoinPool = new ForkJoinPool(4);
+
+
+
+    }
+
+}
